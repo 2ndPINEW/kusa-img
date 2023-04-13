7d92c9a9213d23127c74cad967632b72b16182ce
Eito Takase <eito01178@gmail.com>
2023-04-13 22:59:22 +0900

    cache-control

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const cacheHeaders = [
+  {
+    key: "Cache-Control",
+    value: "max-age=3600, s-maxage=3600",
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  headers: cacheHeaders,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
