7bd0b81427ebd29b57772fed8556da0a5dbec7b5
Eito Takase <eito01178@gmail.com>
2023-06-16 13:29:19 +0900

    contributes を APIにする

--- src/pages/api/contributes.tsx
+++ src/pages/api/contributes.tsx
@@ -1,12 +1,11 @@
 import { retrieveContributionData } from "@/module/github";
-import { ImageResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export const config = {
   runtime: "edge",
 };
 
-export default async function og(req: NextRequest) {
+export default async function handler(req: NextRequest) {
   const { searchParams } = req.nextUrl;
   const userName = searchParams.get("userName");
 
@@ -18,49 +17,9 @@ export default async function og(req: NextRequest) {
 
   const contributes = await retrieveContributionData(userName);
 
-  const { totalContributions, weeks } =
-    contributes.data.user.contributionsCollection.contributionCalendar;
-
-  const size = 20;
-  const margin = 2;
-
-  const width = weeks.length * (size + margin * 2);
-  const height = 7 * (size + margin * 2);
-
-  const weekItems = weeks.map((week) => {
-    return week.contributionDays.map((day) => {
-      return (
-        <div
-          key={day.date}
-          style={{
-            width: `${size}px`,
-            height: `${size}px`,
-            backgroundColor: day.color,
-            margin: `${margin}px`,
-          }}
-        ></div>
-      );
-    });
+  return new Response(JSON.stringify(contributes), {
+    headers: {
+      "content-type": "application/json",
+    },
   });
-
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          background: "white",
-          width: `${width}px`,
-          height: `${height}px`,
-          display: "flex",
-          flexDirection: "column",
-          flexWrap: "wrap",
-        }}
-      >
-        {weekItems}
-      </div>
-    ),
-    {
-      width: width,
-      height: height,
-    }
-  );
 }
